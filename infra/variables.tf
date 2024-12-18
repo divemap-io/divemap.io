1b40c78898f8181a0e1f5be37843a3161f620ed9
pmaga <pawel.p.maga@gmail.com>
2024-12-18 01:22:34 +0100

    feat(terraform): add DNS records by using cloudflare provider

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   type        = string
 }
 
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+  sensitive   = true
+}
+
+################################################################################
+# S3 & Cloudflare
+################################################################################
+
 variable "landing_page_website_domain_name" {
   description = "The domain associated with the landing page."
   type        = string
@@ -14,9 +24,11 @@ variable "app_website_domain_name" {
 }
 
 variable "landing_page_files_path" {
-  type = string
+  description = "The path to directory containing all landing page files"
+  type        = string
 }
 
 variable "app_page_files_path" {
-  type = string
+  description = "The path to directory containing all app service files"
+  type        = string
 }
